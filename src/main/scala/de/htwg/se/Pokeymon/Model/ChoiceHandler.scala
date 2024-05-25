95c081e9ccb01090380df416823970add534e0db
UbaMarvin <ubamarvin95@gmail.com>
2024-05-25 20:03:37 +0200

    add statusStrategy.scala, edit choiceHandeler.scala

--- src/main/scala/de/htwg/se/Pokeymon/Model/ChoiceHandler.scala
+++ src/main/scala/de/htwg/se/Pokeymon/Model/ChoiceHandler.scala
@@ -6,7 +6,7 @@ import de.htwg.se.Pokeymon.Model.Setup.opponent
 
 // pass down the "PlayersChoice" Object containing the trainers with their choices
 // Als eigene Klasse, da so erweiterbar
-case class PlayersChoice(player: Trainer, opponent: Trainer, MSg: String)
+case class PlayersChoice(player: Trainer, opponent: Trainer, roundReport: String)
 
 /*PlayerChoice will be evaluated in the following order:
     change pokemon
@@ -24,22 +24,26 @@ case class SwitchPokemonHandler(nextHandler: ChoiceHandler) extends ChoiceHandle
     copy(nextHandler = succesor)
 
   override def handleChoice(playersChoice: PlayersChoice): PlayersChoice =
-    println("SwitchPkHandler \n")
+    // println("SwitchPkHandler \n")
     val playerChoice = playersChoice.player.choice
     val opponentChoice = playersChoice.opponent.choice
     val player = playersChoice.player
     val opponent = playersChoice.opponent
     val defaultPokemon = player.currentPokemon
+    val oldPlayerMon = player.currentPokemon
+    val oldOppMon = opponent.currentPokemon
 
     (playerChoice, opponentChoice) match {
       // Both players switch their Pokémon
       case (Some(pc: SwitchPokemonChoice), Some(oc: SwitchPokemonChoice)) =>
-        println("both switch")
+        // println("both switch")
         (pc.pokemon, oc.pokemon) match {
           case (Some(playerPokemon), Some(opponentPokemon)) =>
             val upd_player = playersChoice.player.switchPokemon(playerPokemon)
             val upd_opponent = playersChoice.opponent.switchPokemon(opponentPokemon)
-            val updchoice = new PlayersChoice(upd_player, upd_opponent, "\nBoth switched their pokemon")
+            val msgPlayer = "Player chooses " + playerPokemon.name + " to fight!\n"
+            val msgOpp = "Opponent chooses " + opponentPokemon.name + " to fight!\n"
+            val updchoice = new PlayersChoice(upd_player, upd_opponent, msgPlayer + msgOpp)
             nextHandler.handleChoice(updchoice)
           case _ =>
             playersChoice
@@ -47,11 +51,12 @@ case class SwitchPokemonHandler(nextHandler: ChoiceHandler) extends ChoiceHandle
 
       // Only the player switches their Pokémon
       case (Some(pc: SwitchPokemonChoice), _) =>
-        println("player switches")
+        // println("player switches")
         pc.pokemon match {
           case Some(playerPokemon) =>
             val upd_player = playersChoice.player.switchPokemon(playerPokemon)
-            val updchoice = new PlayersChoice(upd_player, playersChoice.opponent, "\nPlayer switched their pokemon")
+            val msgPlayer = "Player chooses " + playerPokemon.name + " to fight!\n"
+            val updchoice = new PlayersChoice(upd_player, playersChoice.opponent, msgPlayer)
             nextHandler.handleChoice(updchoice)
           case None =>
             playersChoice
@@ -59,11 +64,13 @@ case class SwitchPokemonHandler(nextHandler: ChoiceHandler) extends ChoiceHandle
 
       // Only the opponent switches their Pokémon
       case (_, Some(oc: SwitchPokemonChoice)) =>
-        println("opponent switches")
+        // println("opponent switches")
         oc.pokemon match {
           case Some(opponentPokemon) =>
             val upd_opponent = playersChoice.opponent.switchPokemon(opponentPokemon)
-            val updchoice = new PlayersChoice(playersChoice.player, upd_opponent, "\nOpponent switched their pokemon")
+            val msgOpp = "Opponent chooses " + opponentPokemon.name + " to fight!\n"
+
+            val updchoice = new PlayersChoice(playersChoice.player, upd_opponent, msgOpp)
             nextHandler.handleChoice(updchoice)
           case None =>
             playersChoice
@@ -71,7 +78,7 @@ case class SwitchPokemonHandler(nextHandler: ChoiceHandler) extends ChoiceHandle
 
       // No switch, pass to the next handler in the chain
       case _ =>
-        println("no switch")
+        // println("no switch")
         nextHandler.handleChoice(playersChoice)
     }
 
@@ -83,7 +90,7 @@ case class UseItemHandler(nextHandler: ChoiceHandler) extends ChoiceHandler:
     copy(nextHandler = succesor)
 
   override def handleChoice(playersChoice: PlayersChoice): PlayersChoice =
-    println("UseItemHandler \n")
+    // println("UseItemHandler \n")
 
     if (false) { // No Items available yet
       playersChoice
@@ -116,14 +123,14 @@ case class EvaluateAttackHandler(nextHandler: ChoiceHandler) extends ChoiceHandl
       val trainer_rem = trainer.removePokemon(currentPokemon.name)
       val next_pokemon = trainer_rem.getNextPokemon()
       val upd_trainer = trainer_rem.setCurrentPokemon(next_pokemon)
-      println("\nfirst case\n")
+      // println("\nfirst case\n")
       upd_trainer
     } else if (!currentPokemon.isAlive() && trainer.pokemons.size == 10) {
-      println("\nsecond case\n")
+      // println("\nsecond case\n")
       val upd_Trainer = trainer.removePokemon(currentPokemon.name)
       (upd_Trainer)
     } else {
-      println("\nthird case\n")
+      // println("\nthird case\n")
       trainer
     }
 
@@ -131,21 +138,22 @@ case class EvaluateAttackHandler(nextHandler: ChoiceHandler) extends ChoiceHandl
     copy(nextHandler = succesor)
 
   override def handleChoice(playersChoice: PlayersChoice): PlayersChoice =
-    println("EvalMoveHandler \n")
+    // println("EvalMoveHandler \n")
     val playerChoice = playersChoice.player.choice
     val opponentChoice = playersChoice.opponent.choice
     val player = playersChoice.player
     val opponent = playersChoice.opponent
     val playerMon = player.currentPokemon
     val oppMon = opponent.currentPokemon
+    val report = playersChoice.roundReport
 
-    // instanciate Mediator
-    val AttackMediator = new AttackMediator
+    // instanciate AttackContextClass
+    val attackContext = new AttackStrategyContext()
 
     (playerChoice, opponentChoice) match {
       // Both players switch their Pokémon
       case (Some(pc: AttackChoice), Some(oc: AttackChoice)) =>
-        println("both Attack")
+        // println("both Attack")
         (pc.move, oc.move) match {
           case (Some(playerMove), Some(opponentMove)) =>
             // get Pokemon Id for identification
@@ -155,17 +163,22 @@ case class EvaluateAttackHandler(nextHandler: ChoiceHandler) extends ChoiceHandl
             val first = determineFasterPokemon(playerMon, oppMon)
             val second = determineSlowerPokemon(playerMon, oppMon)
             // Faster Pokemon attacks slower onea
-            val (firstPk, secondPk, msgOne) = AttackMediator.inflictDamage(first, second, playerMove)
+            // Set the Context / which attack Strategy to use
+            val updAttackContext = attackContext.setContext(first.currentMove) // not PlayerMove
+            val (firstPk, secondPk, msgOne) = updAttackContext.applyAttackStrategy(first, second, playerMove)
+            // Set the attack Context for the slower Mover
+            val SecondMoverAttackContext = updAttackContext.setContext(second.currentMove) /// Not player Move
             // Slower Pokemon attacks Faster one only if Slower is alive, else "nothing happens"
             val (updFirstPk, updSecondPk, msgTwo) =
-              if (secondPk.isAlive()) then AttackMediator.inflictDamage(secondPk, firstPk, opponentMove) else (firstPk, secondPk, "fainted")
+              if (secondPk.isAlive()) then SecondMoverAttackContext.applyAttackStrategy(secondPk, firstPk, opponentMove)
+              else (firstPk, secondPk, secondPk.name + " fainted!")
 
             // Match Pokemons by Id and link them back to their trainers
             val (updPlayerMon, updOppMon) = matchPokemonById(updFirstPk, updSecondPk, playerPkId, oppPkId)
             // Now check if one has died
             val updPlayer = switchIfdead(player.updateCurrentPokemon(updPlayerMon))
             val updOpp = switchIfdead(opponent.updateCurrentPokemon(updOppMon))
-            val updPlayersChoice = new PlayersChoice(updPlayer, updOpp, "")
+            val updPlayersChoice = new PlayersChoice(updPlayer, updOpp, report + msgOne + msgTwo)
             nextHandler.handleChoice(updPlayersChoice)
 
           case _ =>
@@ -174,13 +187,15 @@ case class EvaluateAttackHandler(nextHandler: ChoiceHandler) extends ChoiceHandl
 
       // Only the player attacks
       case (Some(pc: AttackChoice), _) =>
-        println("player attacks")
+        // println("player attacks")
         pc.move match {
           case Some(move) =>
-            val (updPlayerMon, updOppMon, msg) = AttackMediator.inflictDamage(playerMon, oppMon, move)
+            // Set and apply Attack Context
+            val updAttackContext = attackContext.setContext(move)
+            val (updPlayerMon, updOppMon, msg) = updAttackContext.applyAttackStrategy(playerMon, oppMon, move)
             val updPlayer = player.updateCurrentPokemon(updPlayerMon)
             val updOpp = opponent.updateCurrentPokemon(updOppMon)
-            val updchoice = new PlayersChoice(updPlayer, updOpp, msg)
+            val updchoice = new PlayersChoice(updPlayer, updOpp, report + msg)
             println(msg)
             nextHandler.handleChoice(updchoice)
           case None =>
@@ -189,13 +204,14 @@ case class EvaluateAttackHandler(nextHandler: ChoiceHandler) extends ChoiceHandl
 
       // Only the opponent switches their Pokémon
       case (_, Some(oc: AttackChoice)) =>
-        println("opponent attacks")
+        // println("opponent attacks")
         oc.move match {
           case Some(move) =>
-            val (updOppMon, updPlayerMon, msg) = AttackMediator.inflictDamage(oppMon, playerMon, move)
+            val updAttackContext = attackContext.setContext(move)
+            val (updOppMon, updPlayerMon, msg) = updAttackContext.applyAttackStrategy(oppMon, playerMon, move)
             val updPlayer = player.updateCurrentPokemon(updPlayerMon)
             val updOpp = opponent.updateCurrentPokemon(updOppMon)
-            val updchoice = new PlayersChoice(updPlayer, updOpp, msg)
+            val updchoice = new PlayersChoice(updPlayer, updOpp, report + msg)
             println(msg)
 
             nextHandler.handleChoice(updchoice)
@@ -205,19 +221,25 @@ case class EvaluateAttackHandler(nextHandler: ChoiceHandler) extends ChoiceHandl
 
       // No switch, pass to the next handler in the chain
       case _ =>
-        println("no attacks")
+        // println("no attacks")
         nextHandler.handleChoice(playersChoice)
     }
 
-//no class var because this is the last handler, for now
+//This is the last handler
+//Status effects burned or poisoned are  applied last
 case class StatusHandler() extends ChoiceHandler:
   override def setSuccessor(succesor: ChoiceHandler): ChoiceHandler =
     this
 
   override def handleChoice(playersChoice: PlayersChoice): PlayersChoice =
-    println("StatusHandler \n")
-    // identify Choice
-    // Perform Choice
-    // UpdateModel
-    //
-    playersChoice
+    val player = playersChoice.player
+    val opponent = playersChoice.opponent
+    val playerMon = player.currentPokemon
+    val oppMon = opponent.currentPokemon
+    val report = playersChoice.roundReport
+    val (upd_playerMon, msgOne) = playerMon.status.applyEffect(playerMon)
+    val (upd_oppMon, msgTwo) = oppMon.status.applyEffect(oppMon)
+    val updPlayer = player.updateCurrentPokemon(upd_playerMon)
+    val updOpp = opponent.updateCurrentPokemon(upd_oppMon)
+
+    new PlayersChoice(updPlayer, updOpp, report + msgOne + msgTwo)
---
+++ src/main/scala/de/htwg/se/Pokeymon/Model/StatusStrategy.scala
@@ -0,0 +1,131 @@
+package de.htwg.se.Pokeymon.Model
+import de.htwg.se.Pokeymon.Model
+// Status Strategy
+// Pokemon can be with no status, burned, poisoned, asleep or paralyzed
+// Based on the status the POkemon will behave differently befor or after it executes its move
+// here i will focus solely on what will happen after the move
+// focusing on how the status applys before the move requires a little bit more overhead (and time)
+
+//On before
+//Pokemon get additional instanz val: val boolean canMove
+//if Pokemon is asleep or paralyzed canMove is set to false
+//Attack Mediator / Attack Strategy checks if Pokemon can move and if not extracts from the status what its status is
+// >>> "Pikachu is asleep"
+
+trait StatusStrategy {
+  def statusName: String
+  def duration: Int
+  def applyEffect(pokemon: Pokemon): (Pokemon, String)
+  def clearEffect(pokemon: Pokemon): Pokemon
+  def reduceDuration(statusStrategy: StatusStrategy): StatusStrategy
+}
+
+case class StatusEffectStrategyContext(strategy: StatusStrategy = new NormalState) {
+  def setStatusContext(newStrategy: StatusStrategy): StatusEffectStrategyContext =
+    this.copy(strategy = newStrategy)
+
+  def applyEffect(pokemon: Pokemon): (Pokemon, String) =
+    strategy.applyEffect(pokemon)
+
+  def clearEffect(pokemon: Pokemon): Pokemon =
+    strategy.clearEffect(pokemon)
+
+  def getStatusName(): String = strategy.statusName
+}
+
+// return the Pokemon unharmed
+case class NormalState() extends StatusStrategy {
+  override def duration: Int = 0
+  override def statusName: String = ""
+
+  override def applyEffect(pokemon: Pokemon): (Pokemon, String) =
+    (pokemon, "")
+  override def clearEffect(pokemon: Pokemon): Pokemon =
+    pokemon
+  override def reduceDuration(statusStrategy: StatusStrategy): StatusStrategy =
+    statusStrategy
+}
+
+// Poisoned and Burned are applied at the end of the turn.
+
+//__________Burned
+// @duration: Since a Pokemnn cant be burned forever.
+// @Intensity: Intensity at which it burns. Not all burns are equally intense, some may change over time?
+case class BurnedState(duration: Int, intensity: Int = 5) extends StatusStrategy {
+  override def statusName: String = "burned"
+
+  override def applyEffect(pokemon: Pokemon): (Pokemon, String) =
+    // apply effect
+    val damagedPokemon = pokemon.decreaseHp(intensity)
+    val msg = pokemon.name + " is hurt by burn, lost " + intensity + " HP\n"
+    // reduceDuration
+    val updStatusStrategy = reduceDuration(this)
+    // Re-initialize based on duration
+    val updPokemon = if (updStatusStrategy.duration > 0) { damagedPokemon.setStatus(updStatusStrategy) }
+    else { damagedPokemon.setStatus(new NormalState) }
+    (updPokemon, msg)
+
+  override def reduceDuration(statusStrategy: StatusStrategy): StatusStrategy =
+    copy(duration = duration - 1)
+
+  override def clearEffect(pokemon: Pokemon): Pokemon =
+    pokemon.copy(status = new StatusEffectStrategyContext)
+}
+
+//_______Poisoned
+case class PoisonedState(duration: Int, intensity: Int = 5) extends StatusStrategy {
+  override def statusName: String = "poisoned"
+
+  override def applyEffect(pokemon: Pokemon): (Pokemon, String) =
+    // apply effect
+    val damagedPokemon = pokemon.decreaseHp(intensity)
+    val msg = pokemon.name + " is hurt by poison, lost " + intensity + " HP\n"
+
+    // reduceDuration
+    val updStatusStrategy = reduceDuration(this)
+    // Re-initialize based on duration
+    val updPokemon = if (updStatusStrategy.duration > 0) { damagedPokemon.setStatus(updStatusStrategy) }
+    else { damagedPokemon.setStatus(new NormalState) }
+    (updPokemon, msg)
+
+  override def reduceDuration(statusStrategy: StatusStrategy): StatusStrategy =
+    copy(duration = duration - 1)
+
+  override def clearEffect(pokemon: Pokemon): Pokemon =
+    pokemon.copy(status = new StatusEffectStrategyContext)
+}
+
+// Sleep and Paralyzed are applied at the beginning of AttackChoiceHandler -> attackStrategy.inflict damage
+// If status state is sleep state then cant move oder sooo
+case class SleepState(duration: Int) extends StatusStrategy {
+  override def statusName: String = "sleep"
+  override def applyEffect(pokemon: Pokemon): (Pokemon, String) =
+    // reduceDuration
+    val msg = pokemon.name + " is sleeping\n"
+    val updStatusStrategy = reduceDuration(this)
+    val updPokemon = if (updStatusStrategy.duration > 0) { pokemon.setStatus(updStatusStrategy) }
+    else { pokemon.setStatus(new NormalState) }
+    (updPokemon, msg)
+
+  override def reduceDuration(statusStrategy: StatusStrategy): StatusStrategy =
+    copy(duration = duration - 1)
+
+  override def clearEffect(pokemon: Pokemon): Pokemon =
+    pokemon.copy(status = new StatusEffectStrategyContext)
+}
+
+case class ParalyzedState(duration: Int) extends StatusStrategy {
+  override def statusName: String = "paralyzed"
+  override def applyEffect(pokemon: Pokemon): (Pokemon, String) =
+    // reduceDuration
+    val updStatusStrategy = reduceDuration(this)
+    val msg = pokemon.name + " is paralyzed\n"
+    val updPokemon = if (updStatusStrategy.duration > 0) { pokemon.setStatus(updStatusStrategy) }
+    else { pokemon.setStatus(new NormalState) }
+    (updPokemon, msg)
+  override def reduceDuration(statusStrategy: StatusStrategy): StatusStrategy =
+    copy(duration = duration - 1)
+
+  override def clearEffect(pokemon: Pokemon): Pokemon =
+    pokemon.copy(status = new StatusEffectStrategyContext)
+}
